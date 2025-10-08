1810a8ce23d072a3ea27880205db39f6797eeecc
sontixyou <kengoviolinphoto@gmail.com>
2025-10-09 07:13:17 +0900

    feat: first commit

---
+++ init.lua
@@ -0,0 +1,58 @@
+-- require("config.lazy")
+-- require("config.autocmds")
+-- require("config.format")
+
+local opt = vim.opt
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+opt.swapfile = false
+-- 新しい行を追加したいときに、一つ上の行のインデント引き継ぎます
+opt.autoindent = true
+opt.smartindent = true
+opt.shiftround = true -- Round indent
+opt.shiftwidth = 2 -- Size of an indent
+opt.tabstop = 2 -- Number of spaces tabs count for
+opt.expandtab = true
+opt.softtabstop = 2
+opt.spelllang = "en_us"
+opt.spell = true
+opt.relativenumber = true
+opt.mouse = "a"
+opt.clipboard = "unnamedplus" -- Sync with system clipboard
+opt.splitright = true -- Put new windows right of current
+opt.termguicolors = true -- True color support
+opt.undofile = true
+opt.undolevels = 10000
+opt.smoothscroll = true
+
+
+-- keymap
+keymap.set("i", "jj", "<ESC>", opts)
+keymap.set("n", ";", ":", opts)
+-- Terminal
+keymap.set("t", "jj", "<c-\\><c-n>:close<cr>", { nowait = true })
+-- Window
+keymap.set("n", "<leader>sv", "<C-w>v", opts) -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", opts) -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", opts) -- make split window equal width
+keymap.set("n", "<leader>sx", ":close<CR>", opts) -- close current split window
+-- 簡易的にbinding.bを入力できるようにする
+keymap.set("n", ",b", "odebugger<Esc>", opts)
+keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
+
+keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to lower window" })
+keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to upper window" })
+keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+
+-- better indenting
+keymap.set("v", "<", "<gv")
+keymap.set("v", ">", ">gv")
+
+vim.g['denops#deno'] = '/opt/homebrew/bin/deno'
+
+-- Load necromancer plugins
+local plugin_dir = vim.fn.expand('~/.local/share/nvim/necromancer/plugins')
+for _, plugin in ipairs(vim.fn.readdir(plugin_dir)) do
+  vim.opt.runtimepath:append(plugin_dir .. '/' .. plugin)
+end
