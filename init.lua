e6878b925cb66a5f82560bccf5376c715b6469e9
sontixyou <kengoviolinphoto@gmail.com>
2025-10-16 22:38:51 +0900

    feat: add bufferline config

--- init.lua
+++ init.lua
@@ -190,6 +190,73 @@ require('treesj').setup({})
 -- Configure autoclose.nvim
 require("autoclose").setup()
 
+-- Configure bufferline.nvim
+require('bufferline').setup {
+  options = {
+    mode = "buffers",
+    themable = true,
+    numbers = "none",
+    close_command = "bdelete! %d",
+    right_mouse_command = "bdelete! %d",
+    left_mouse_command = "buffer %d",
+    middle_mouse_command = nil,
+    indicator = {
+      icon = '▎',
+      style = 'icon',
+    },
+    buffer_close_icon = '󰅖',
+    modified_icon = '● ',
+    close_icon = '',
+    left_trunc_marker = '',
+    right_trunc_marker = '',
+    max_name_length = 18,
+    max_prefix_length = 15,
+    truncate_names = true,
+    tab_size = 18,
+    diagnostics = "nvim_lsp",
+    diagnostics_update_in_insert = false,
+    diagnostics_update_on_event = true,
+    diagnostics_indicator = function(count, level, diagnostics_dict, context)
+      local icon = level:match("error") and " " or " "
+      return " " .. icon .. count
+    end,
+    offsets = {
+      {
+        filetype = "neo-tree",
+        text = "File Explorer",
+        text_align = "left",
+        separator = true
+      }
+    },
+    color_icons = true,
+    show_buffer_icons = true,
+    show_buffer_close_icons = true,
+    show_close_icon = true,
+    show_tab_indicators = true,
+    show_duplicate_prefix = true,
+    persist_buffer_sort = true,
+    separator_style = "thin",
+    enforce_regular_tabs = false,
+    always_show_bufferline = true,
+    hover = {
+      enabled = true,
+      delay = 200,
+      reveal = {'close'}
+    },
+    sort_by = 'insert_after_current',
+  }
+}
+
+-- Bufferline keybindings
+keymap.set('n', '<S-h>', '<cmd>BufferLineCyclePrev<cr>', { desc = 'Previous buffer' })
+keymap.set('n', '<S-l>', '<cmd>BufferLineCycleNext<cr>', { desc = 'Next buffer' })
+keymap.set('n', '<leader>bp', '<cmd>BufferLineTogglePin<cr>', { desc = 'Toggle pin buffer' })
+keymap.set('n', '<leader>bP', '<cmd>BufferLineGroupClose ungrouped<cr>', { desc = 'Delete non-pinned buffers' })
+keymap.set('n', '<leader>bo', '<cmd>BufferLineCloseOthers<cr>', { desc = 'Delete other buffers' })
+keymap.set('n', '<leader>br', '<cmd>BufferLineCloseRight<cr>', { desc = 'Delete buffers to the right' })
+keymap.set('n', '<leader>bl', '<cmd>BufferLineCloseLeft<cr>', { desc = 'Delete buffers to the left' })
+keymap.set('n', 'gb', '<cmd>BufferLinePick<cr>', { desc = 'Pick buffer' })
+
 -- Configure gitsigns.nvim
 require('gitsigns').setup {
   signs = {
