b340eb44974752443c0ec44b73fb92e247b4b916
sontixyou <kengoviolinphoto@gmail.com>
2025-10-16 14:47:56 +0900

    feat: change find_files of telescope config

--- init.lua
+++ init.lua
@@ -119,7 +119,9 @@ require'nvim-treesitter'.setup {
 }
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader><leader>', function()
+  builtin.find_files({ hidden = true })
+end, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
